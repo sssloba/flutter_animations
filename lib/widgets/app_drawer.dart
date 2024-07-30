398d5edf4b79dcd701441fa49c109cce649cdd6c
sssloba <slobodan.stojakov@gmail.com>
2024-07-30 23:30:19 +0200

    app-drawer

---
+++ lib/widgets/app_drawer.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppDrawer extends StatelessWidget {
+  const AppDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: const [Text('Demo Tile')],
+      ),
+    );
+  }
+}
